f66145db11b18ced97387561068c605518ed0ffb
Игорь <>
2016-09-24 17:03:46 +0300

    return

--- m/main.js
+++
@@ -1,9 +0,0 @@
-console.log(1);
-
-$('button').click(function(){
-	$('.test').addClass('animation');
-});
-
-var arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
-var arr2 = arr.splice(-3);
-console.log(arr2);
